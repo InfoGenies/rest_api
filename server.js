d9e6940d27ca1765d2e24f893ffc78581862854a
InfoGenie <asma.hami@univ-constantine.dz>
2023-05-18 10:17:02 +0100

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http')
 const app = require('./app')
 const server = http.createServer(app)
-server.listen(3000, '127.0.0.1', () => {
+const port = process.env.Port || 3000
+server.listen(port, '127.0.0.1', () => {
     console.log('server is running')
 })
 
