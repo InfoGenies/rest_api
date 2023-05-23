bab382993e090381832c4e8bf2891c3263a24847
InfoGenie <asma.hami@univ-constantine.dz>
2023-05-23 16:19:15 +0100

    first commit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(morgan('dev'));
 // make the file that contain all images is accessible  (permission)
 app.use(express.static('public'));
 // 
-app.use(fileUpload({
+app.use(fileupload({
     useTempFiles: true,
     tempFileDir: "/tmp",
     limits: {
