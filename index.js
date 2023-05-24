d72975cd6775417683e312e2e4ef260a513d21a1
InfoGenie <asma.hami@univ-constantine.dz>
2023-05-24 16:47:28 +0100

    second commit

--- index.js
+++ index.js
@@ -53,7 +53,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(fileupload({
-    tempFileDir: "/tmp"
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+    limits: {
+      fileSize: 1024 * 1024 * 5 // 1 MB in bytes
+    }
   }));
 // *use()* is Middleware, which are functions that can be executed before or after a request is
 // processed. Middleware can be used to handle common tasks such 
